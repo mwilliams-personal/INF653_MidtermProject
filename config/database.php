4a6a9f6d4a5196c53498cd831a07525b21f71bf2
Matt Williams <lpugfan2004@gmail.com>
2022-03-04 09:24:07 -0500

    updating the database code to remove the constructor and add variables to connect method

--- config/database.php
+++ config/database.php
@@ -1,30 +1,24 @@
 <?php 
   class Database {
     
-    protected $host;
-    protected $uername;
-    protected $password;
-    protected $db_name;
-
-    function __construct(){
-        $jawsurl = getenv('JAWSDB_URL');
-        $dbparts = parse_url($jawsurl);
-        
-        // DB Params
-        $this->host = $dbparts['host'];
-        $this->username = $dbparts['user'];
-        $this->password = $dbparts['pass'];
-        $this->db_name = ltrim($dbparts['path'],'/');
-    }
-    
     public $conn;
 
     // DB Connect
     public function connect() {
+
+      $jawsurl = getenv('JAWSDB_URL');
+      $dbparts = parse_url($jawsurl);
+        
+      // DB Params
+      $host = $dbparts['host'];
+      $username = $dbparts['user'];
+      $password = $dbparts['pass'];
+      $db_name = ltrim($dbparts['path'],'/');
+
       $this->conn = null;
 
       try { 
-        $this->conn = new PDO('mysql:host=' . $this->host . ';dbname=' . $this->db_name, $this->username, $this->password);
+        $this->conn = new PDO('mysql:host=' . $host . ';dbname=' . $db_name, $username, $password);
         $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       } catch(PDOException $e) {
         echo 'Connection Error: ' . $e->getMessage();
